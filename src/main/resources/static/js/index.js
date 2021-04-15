c9ca9d3555b2dfc44347b37cd13678fc3ac28113
taehee-kim-dev <taehee.kim.dev@gmail.com>
2021-04-16 03:09:25 +0900

    feat: 방 생성시, white 플레이어 비밀번호 입력 부분 추가

--- src/main/resources/static/js/index.js
+++ src/main/resources/static/js/index.js
@@ -19,14 +19,22 @@ for (let i = 0; i < remove_room_buttons.length; i++) {
   });
 }
 
-const room_title_min_size = 5;
 function create_room_check() {
-  const new_room_title = document.getElementById('new_room_title');
+  const room_title_min_size = 5;
 
+  const new_room_title = document.getElementById('new_room_title');
   if(new_room_title.value.length < room_title_min_size) {
     alert('방 제목은 ' + room_title_min_size + '글자 이상이어야 합니다.');
     new_room_title.focus();
     return false;
   }
+
+  const white_player_password = document.getElementById('white-player-password');
+  if(white_player_password.value.length < room_title_min_size) {
+    alert('비밀번호는 ' + room_title_min_size + '자 이상이어야 합니다.');
+    white_player_password.focus();
+    return false;
+  }
+
   return true;
 }
